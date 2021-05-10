ed9443b2fec40ee2b0af369ea3206ff2db7ea762
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-11 01:09:19 +0200

    started setting up LinkedList.java

--- src/dataStructures/LinkedList.java
+++ src/dataStructures/LinkedList.java
@@ -1,5 +1,22 @@
 package dataStructures;
 
-public class LinkedList {
+public class LinkedList<E> {
+
+    //Attributes
+    private Node<E> first; //creating the first node
+    int size; //size (length) of the list
+
+    //Constructors
+    public LinkedList() {
+        //empty constructor
+        //We may add it or assume its existence
+    }
+
+    //Methods
+    //Getters and Setters
+    public Node<E> getFirst() {
+        return first;
+    }
+
     
 }
