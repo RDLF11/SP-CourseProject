6bf0cc2620877cd3ccd155ba3bf90e50d6ed9b0f
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-11 01:47:24 +0200

    Update LinkedList.java
    added:
    setFirst
    isEmpty
    size
    insert new node first
    extract the first node
    
    insert new node taking into account previous

--- src/dataStructures/LinkedList.java
+++ src/dataStructures/LinkedList.java
@@ -18,5 +18,44 @@ public class LinkedList<E> {
         return first;
     }
 
+    public void setFirst(Node<E> first) {
+        this.first = first; //assign first node
+    }
+
+    //check if the LL is empty
+    public boolean isEmpty() {
+        return (first == null);
+        //returns based on first node status of existence
+    }
+
+    public int size() {
+        return size; //returns the size of the LL
+    }
     
+    public void insert(E info) {
+        Node<E> node = new Node<E>(info); //created new node (object)
+        node.setNext(first); //set as first as reference
+        first = node;
+        size++; //size post-increment
+    }
+
+    //method to extract a node from an LL
+    public E extract() {
+        E info = null;
+        if(first != null) {
+            info = first.getInfo();
+            first = first.getNext();
+            size--;
+        }
+        return info;
+    }
+
+    public void insert(E info, Node<E> prev) {
+        if (prev != null) {
+            Node<E> node = new Node<E>(info);
+            node.setNext(prev.getNext());
+            prev.setNext(node);
+            size++; 
+        }
+    }
 }
