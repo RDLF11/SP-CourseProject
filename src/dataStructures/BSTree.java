b10c2ce841269eb9069813d06fa957f5f2dd5865
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-11 02:52:16 +0200

    Various changes

--- src/dataStructures/BSTree.java
+++ src/dataStructures/BSTree.java
@@ -1,5 +1,28 @@
 package dataStructures;
 
-public class BSTree {
+//recompilation of method declarations
+public interface BSTree<E> {
     
+    //does the tree exist?
+    boolean isEmpty();
+
+    //get node info
+    E getInfo();
+
+    //gets node's key
+    Comparable getKey();
+
+    //get R or L node
+    BSTree<E> getLeft();
+    BSTree<E> getRight();
+
+    String toStringPreOrder(); //count when stop on left
+    String toStringInOrder(); //count when stop below node
+    String toStringPostOrder(); //count when stop after
+
+    String toString(); //pre-order
+
+    void insert(Comparable key, E info);
+
+    BSTree<E> search(Comparable key);
 }
--- src/dataStructures/BSTreeException.java
+++ src/dataStructures/BSTreeException.java
@@ -6,5 +6,12 @@ public class BSTreeException extends Exception {
      *
      */
     private static final long serialVersionUID = 1L;
+
+    public BSTreeException(String message) {
+        super(message);
+        //prints out error message
+        //if confused refer to super implementation
+        //of the 1 parameter constructor of Exception
+    }
     
 }
