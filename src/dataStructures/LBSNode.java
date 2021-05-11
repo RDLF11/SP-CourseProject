7cead866919e7aeefe1e2ae09905b075fc37418f
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-11 03:04:30 +0200

    reodered files, finished data structures

--- src/dataStructures/LBSNode.java
+++ src/dataStructures/LBSNode.java
@@ -1,5 +1,68 @@
 package dataStructures;
 
-public class LBSNode {
-    
+public class LBSNode<E> {
+
+    //Attributes
+    private E info;
+    private Comparable  key;
+    private BSTree<E> right;
+    private BSTree<E> left;
+
+    //Constructor
+    public LBSNode(Comparable  key, E info, BSTree<E> left, BSTree<E> right) {
+        this.info = info;
+        this.key = key;
+        this.right = right;
+        this.left = left;
+    }
+
+    //Methods
+    //Get node info
+    public E getInfo() {
+        return this.info;
+    }
+
+    //set node info
+    public void setInfo(E info) {
+        this.info = info;
+    }
+
+    //get node's key
+    public Comparable getKey() {
+        return this.key;
+    }
+
+    //set node's key
+    public void setKey(Comparable  key) {
+        this.key = key;
+    }
+
+    //get left (lower) node
+    public BSTree<E> getLeft() {
+        return this.left;
+    }
+
+    //setLeft node
+    public void setLeft(BSTree<E> left) {
+        this.left = left;
+    }
+
+    //get right (lower) node
+    public BSTree<E> getRight() {
+        return this.right;
+    }
+
+    //setRight node
+    public void setRight(BSTree<E> right) {
+        this.right = right;
+    }
+
+    //toString method
+    public String toString() {
+        String resultado = "";
+        if (info != null) {
+            resultado = info.toString();
+        }
+        return resultado;
+    }
 }
