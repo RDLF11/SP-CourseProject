fa48223fc72cd13f2de7b82d98377f4c921cdbc3
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-11 02:28:05 +0200

    finished Queue.jave

--- src/dataStructures/Queue.java
+++ src/dataStructures/Queue.java
@@ -1,5 +1,22 @@
 package dataStructures;
 
-public class Queue {
+public interface Queue<E> {
     
+    //friendly values (implicit visibility)
+
+    //check if queue even exists
+    boolean isEmpty();
+
+    //Length of teh queue
+    int size();
+
+    //first Node
+    E front();
+
+    //input info value inside the queue
+    void enqueue(E info);
+
+    //Extract the E info value from the queue
+    E dequeue();
+
 }
