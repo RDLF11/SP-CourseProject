847ee14f9f0fba27b338aa16942fb727a26c3309
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-11 02:09:29 +0200

    Update LinkedList.java
    added more methods and various comments though not in the utmost detail

--- src/dataStructures/LinkedList.java
+++ src/dataStructures/LinkedList.java
@@ -32,6 +32,7 @@ public class LinkedList<E> {
         return size; //returns the size of the LL
     }
     
+    //method to insert a first node into an LL
     public void insert(E info) {
         Node<E> node = new Node<E>(info); //created new node (object)
         node.setNext(first); //set as first as reference
@@ -39,7 +40,7 @@ public class LinkedList<E> {
         size++; //size post-increment
     }
 
-    //method to extract a node from an LL
+    //method to extract first node from an LL
     public E extract() {
         E info = null;
         if(first != null) {
@@ -50,12 +51,68 @@ public class LinkedList<E> {
         return info;
     }
 
+    //method to insert a node with a reference to prev node
     public void insert(E info, Node<E> prev) {
-        if (prev != null) {
-            Node<E> node = new Node<E>(info);
-            node.setNext(prev.getNext());
-            prev.setNext(node);
-            size++; 
+        if(prev != null) {
+            Node<E> node = new Node<E>(info); //create new node
+            node.setNext(prev.getNext()); //setNext after previous and getNext fro prev
+            prev.setNext(node); //setNext overridden to newly created node
+            size++;
         }
+    }
+
+    //method to extract a node based on prev node
+    public E extract(Node<E> prev) {
+        E info = null;
+        if(prev != null && prev.getNext() != null) {
+            info = prev.getNext().getInfo();
+            prev.setNext(prev.getNext().getNext())
+            size--;
+        }
+        return info;
+    }
+
+    //extractLast
+    public E extractLast() {
+        E info = null;
+        Node<E> aux = first;
+        if(aux != null && aux.getNext() != null) {
+            while(aux.getNext().getNext() != null) {
+                aux = aux.getNext();
+            }
+            info = aux.getNext().getInfo(); //grabs the absolute last after while loop execution
+            aux.setNext(null);
+        }
+        return info;
+    }
+
+    //Extract node based on its index
+    public E extract(int index) {
+        E info = null;
+        if(index < 0 || index > size - 1) {
+            System.err.println("Invalid index");
+        } else if (isEmpty()) {
+            System.err.println("List is empty");
+        } else {
+            Node<E> aux = first;
+            for(int i = 0; i < index - 1 && aux != null; i++) {
+                aux = aux.getNext();
+            }
+            if(aux.getNext() != null) {
+                info = aux.getNext().getInfo();
+                aux.setNext(aux.getNext().getNext());
+            }
+        }
+        return info;
+    }
+
+    public Node<E> searchNode(E info) {
+        Node<E> node = null;
+        Node<E> aux = first;
+        while(aux != null) {
+            if(aux.getInfo().equals(info)) node = aux;
+            aux = aux.getNext();
+        }
+        return node;
     }
 }
