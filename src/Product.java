0a637e7dd45781c79a3cd787397dd2514f570754
Rodrigo De Lama <42297804+RDLF11@users.noreply.github.com>
2021-05-24 02:06:56 +0200

    created getters and setter in prodcut and added attributes in stockableproduct

--- src/Product.java
+++ src/Product.java
@@ -5,26 +5,61 @@ public class Product {
     //Attributes
 
     //Product components
-    String name;
-    String brand;
-    String category;
-        //Identifiers for the different categories
-        final char FOOD = 'f';
-        final char SUPPLIES = 's';
-        final char EQUIPMENT = 'e';
-        final char MISCELLANY = 'm';
+    private String name;
+    private String brand;
+    private char category;
+            //Identifiers for the different categories
+        static final char FOOD = 'f';
+        static final char SUPPLIES = 's';
+        static final char EQUIPMENT = 'e';
+        static final char MISCELLANY = 'm';
 
     //Countable product?
-    boolean isCountable;
+    private boolean isCountable;
 
     //Measurement units
-    String measurementUnit; //"kg", "liters", "pieces"....
+    private String measurementUnit; //"kg", "liters", "pieces"....
 
     //Constructor
-    public Product(String name, String brand, String category) {
+    public Product(String name, String brand, char category, boolean isCountable, String measurementUnits) {
+        setName(name);
+        setBrand(brand);
+        setCategory(category);
+        setIsCountable(isCountable);
+        setMeasurementUnit(measurementUnit);
+    }
+
+    //Methods
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
         this.name = name;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
         this.brand = brand;
+    }
+    public char getCategory() {
+        return category;
+    }
+    public void setCategory(char category) {
         this.category = category;
+    }
+    public boolean getIsCountable() {
+        return isCountable;
+    }
+    public void setIsCountable(boolean isCountable) {
+        this.isCountable = isCountable;
+    }
+    public String getMeasurementUnit() {
+        return measurementUnit;
+    }
+    public void setMeasurementUnit(String measurementUnit) {
+        this.measurementUnit = measurementUnit;
     }
 
 }
--- src/StockableProduct.java
+++ src/StockableProduct.java
@@ -3,7 +3,15 @@
 public class StockableProduct extends Product {
     
     //Attributes
-    int productID;
-    
+    private int productID;
+    private int numUnits;
+    private double costPerUnit;
+    private double pricePerUnits;
+
+    public StockableProduct() {
+        super(name, brand, category, isCountable, measurementUnit);
+
+    }
+
 
 }
